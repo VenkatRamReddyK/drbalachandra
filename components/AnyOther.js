610b49481d978ae9db7e9e32e53abd36898e4789
Parshuarm Reddy <parshuram.sudda@gmail.com>
2022-01-28 23:05:02 +0530

    Customer reviews Updated

--- components/AnyOther.js
+++ components/AnyOther.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 function AnyOther() {
     return <div>
-        <section class="py-20 bg-white">
-            <div class="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
-                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-5xl">
+        <section className="py-20 bg-white">
+            <div className="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-5xl">
                     Still running something in mind 
                 </h2>
-                <p class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-base md:max-w-3xl">
+                <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-base md:max-w-3xl">
                    Our Faq has some previously asked question check that out or else reach us by clicking the contact button.
                 </p>
-                <div class="flex justify-center mt-8 space-x-3">
-                    <a href="/Contact" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium hover:text-white text-white bg-pink-600 border border-transparent rounded-md shadow hover:bg-pink-700">Contact Us now</a>
-                    <a href="/Faq" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-pink-700 bg-pink-100 border border-transparent rounded-md hover:bg-pink-200">Check out Faq</a>
+                <div className="flex justify-center mt-8 space-x-3">
+                    <a href="/Contact" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-pink-600 border border-transparent rounded-md shadow hover:text-white hover:bg-pink-700">Contact Us now</a>
+                    <a href="/Faq" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-pink-700 bg-pink-100 border border-transparent rounded-md hover:bg-pink-200">Check out Faq</a>
                 </div>
             </div>
         </section>
--- components/Slider.js
+++ components/Slider.js
@@ -4,98 +4,67 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
 export default function MyApp() {
     return (
-        <div className="bg-gray-50">
-            <div className="absolute z-0 flex items-center justify-between w-full">
-                <div className="w-1/3 h-full bg-white" />
-                <div className="w-4/6 h-full ml-16 bg-gray-100" />
-            </div>
-            <div className="relative px-8 py-20 xl:px-20 2xl:mx-auto 2xl:container cursor-pointer">
-                <CarouselProvider naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={3} infinite={true} dragEnabled={true}>
-                    <h1 className="hidden text-5xl font-bold leading-tight text-gray-800 xl:block">
-                        What our customers are
-                        <br />
-                        saying
-                    </h1>
-                    <h1 className="block text-5xl font-bold leading-tight text-gray-800 xl:hidden lg:leading-10">What our customers are saying</h1>
-                    <Slider>
-                        <Slide index={0} tabIndex="null">
-                            <div className="flex">
-                                <div className="mt-14 md:flex">
-                                    <div className="relative lg:w-1/2 sm:w-96 xl:h-96 h-80">
-                                        <img src="Customer/1.jpg" alt="image of profile" className="flex-shrink-0 object-cover w-full h-full rounded shadow-lg object-fit " />
-                                        <div className="absolute top-0 right-0 items-center justify-center hidden w-32 h-32 -mr-16 bg-pink-100 rounded-full md:flex -mt-14">
-                                            <img src="vectorpaint.svg" alt="commas" />
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-between mt-4 md:w-1/3 lg:w-1/3 xl:ml-32 md:ml-20 md:mt-0">
-                                        <div>
-                                            <h1 className="text-2xl font-semibold text-gray-800 xl:leading-loose">Some of the best work that was done!</h1>
-                                            <p className="mt-4 text-base font-medium leading-6 text-gray-600">Our core values are at the heart of all that we do. They are integrated into our daily work lives and help us to remember our customers always comes first, the last thank you should always comes from us.</p>
-                                        </div>
-                                        <div className="mt-8 md:mt-0">
-                                            <p className="text-base font-medium leading-4 text-gray-800">Anna Smith</p>
-                                            <p className="mt-2 mb-4 text-base leading-4 text-gray-600">Senior Web Designer</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Slide>
-                        <Slide index={1}>
-                            <div className="relative flex" style={{ transform: 'translateX(0%)' }}>
-                                <div className="mt-14 md:flex">
-                                    <div className="relative lg:w-1/2 sm:w-96 xl:h-96 h-80">
-                                        <img src="Customer/2.jpg" alt="image of profile" className="flex-shrink-0 object-cover w-full h-full rounded shadow-lg object-fit" />
-                                        <div className="absolute top-0 right-0 items-center justify-center hidden w-32 h-32 -mr-16 bg-pink-100 rounded-full md:flex -mt-14">
-                                            <img src="vectorpaint.svg" alt="commas" />
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-between mt-4 md:w-1/3 lg:w-1/3 xl:ml-32 md:ml-20 md:mt-0">
-                                        <div>
-                                            <h1 className="text-2xl font-semibold text-gray-800 xl:leading-loose">Some of the best work that was done!</h1>
-                                            <p className="mt-4 text-base font-medium leading-6 text-gray-600">Our core values are at the heart of all that we do. They are integrated into our daily work lives and help us to remember our customers always comes first, the last thank you should always comes from us.</p>
-                                        </div>
-                                        <div className="mt-8 md:mt-0">
-                                            <p className="text-base font-medium leading-4 text-gray-800">Anna Smith</p>
-                                            <p className="mt-2 mb-4 text-base leading-4 text-gray-600">Senior Web Designer</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Slide>
-                        <Slide index={0}>
-                            <div className="relative flex" style={{ transform: 'translateX(0%)' }}>
-                                <div className="mt-14 md:flex">
-                                    <div className="relative lg:w-1/2 sm:w-96 xl:h-96 h-80">
-                                        <img src="Customer/3.jpg" alt="image of profile" className="flex-shrink-0 object-cover w-full h-full rounded shadow-lg object-fit" />
-                                        <div className="absolute top-0 right-0 items-center justify-center hidden w-32 h-32 -mr-16 bg-pink-100 rounded-full md:flex -mt-14">
-                                            <img src="vectorpaint.svg" alt="commas" />
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-between mt-4 md:w-1/3 lg:w-1/3 xl:ml-32 md:ml-20 md:mt-0">
-                                        <div>
-                                            <h1 className="text-2xl font-semibold text-gray-800 xl:leading-loose">Some of the best work that was done!</h1>
-                                            <p className="mt-4 text-base font-medium leading-6 text-gray-600">Our core values are at the heart of all that we do. They are integrated into our daily work lives and help us to remember our customers always comes first, the last thank you should always comes from us.</p>
-                                        </div>
-                                        <div className="mt-8 md:mt-0">
-                                            <p className="text-base font-medium leading-4 text-gray-800">Anna Smith</p>
-                                            <p className="mt-2 mb-4 text-base leading-4 text-gray-600">Senior Web Designer</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Slide>
-                    </Slider>
-                    <div className="flex items-center mt-8">
-                        <ButtonBack className="cursor-pointer " role="button" aria-label="previous slide">
-                            <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/testimonal-svg2.svg" alt="previous" />
-                        </ButtonBack>
+        <div className="border-2 border-pink-400 ">
+            <section class="flex items-center justify-center py-20 bg-white min-w-screen">
+                <div class="px-16 bg-white">
+                    <div class="container flex flex-col items-start mx-auto lg:items-center">
+                        <p class="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-pink-500 uppercase lg:justify-center lg:items-center">Don't just take our word for it</p>
 
-                        <ButtonNext role="button" aria-label="next slide" className="ml-2 cursor-pointer">
-                            <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/testimonial-svg3.svg" alt="next" />
-                        </ButtonNext>
+
+                        <h2 class="relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 text-gray-200 lg:inline-block" viewBox="0 0 975.036 975.036">
+                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                                </path>
+                            </svg>
+                            See what others are saying</h2>
+                        <div class="block w-full h-0.5 max-w-lg mt-6 bg-pink-100 rounded-full"></div>
+
+                        <div class="items-center justify-center w-full mt-12 mb-4 lg:flex">
+                            <div class="flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0">
+                                <div class="flex items-center justify-center">
+                                    <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                                        <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1700&amp;q=80" class="object-cover w-full h-full"/>
+                                    </div>
+                                    <div class="flex flex-col items-start justify-center">
+                                        <h4 class="font-bold text-gray-800">John Doe</h4>
+                                        <p class="text-gray-600">CEO of Something</p>
+                                    </div>
+                                </div>
+                                <blockquote class="mt-8 text-lg text-gray-500">"They actually use the ZocDoc's generated PDF document that we fill. Made my day :)"</blockquote>
+                            </div>
+                            <div class="flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-200">
+                                <div class="flex items-center justify-center">
+                                    <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                                        <img src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2547&amp;q=80" class="object-cover w-full h-full"/>
+                                    </div>
+                                    <div class="flex flex-col items-start justify-center">
+                                        <h4 class="font-bold text-gray-800">Jane Doe</h4>
+                                        <p class="text-gray-600">CTO of Business</p>
+                                    </div>
+                                </div>
+                                <blockquote class="mt-8 text-lg text-gray-500">"Dr. BalaChandra is great and the office is conveniently located right off the interstate for super fast visits.
+                                    easier.
+                                    Thanks for making such a great product."</blockquote>
+                            </div>
+                            <div class="flex flex-col items-start justify-start w-full h-auto lg:w-1/3">
+                                <div class="flex items-center justify-center">
+                                    <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                                        <img src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1256&amp;q=80" class="object-cover w-full h-full"/>
+                                    </div>
+                                    <div class="flex flex-col items-start justify-center">
+                                        <h4 class="font-bold text-gray-800">John Smith</h4>
+                                        <p class="text-gray-600">Creator of Stuff</p>
+                                    </div>
+                                </div>
+                                <blockquote class="mt-8 text-lg text-gray-500">"First appointment is a phone call, but seems good so far. Given lab request via email and will follow up.
+                                    looking
+                                    for. I would highly recommend this to anyone."</blockquote>
+                            </div>
+                        </div>
                     </div>
-                </CarouselProvider>
-            </div>
+                </div>
+            </section>
+
         </div>
     );
 }
